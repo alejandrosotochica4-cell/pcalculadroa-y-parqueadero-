0d4132bfae44375e6be2214c08590242eeb71d12
alejandrosotochica4-cell <alejandrosotochica4@gmail.com>
2026-03-16 20:40:36 -0500

    Delete principal.py

--- principal.py
+++
@@ -1,7 +0,0 @@
-from calculadora_controlador import ControladorCalculadora
-
-if __name__ == "__main__":
-    print(" CALCULADORA - Estructura MVC")
-
-    controlador = ControladorCalculadora()
-    controlador.vista.iniciar()
